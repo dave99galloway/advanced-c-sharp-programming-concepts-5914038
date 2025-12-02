7d54b98c22dc6bb05b6eb635c11bd553663c4b58
Dave Galloway <dave.00.galloway@gmail.com>
2025-12-02 17:00:57 +0000

    solved event challenge

--- Start/Events/EventChallenge/Program.cs
+++ Start/Events/EventChallenge/Program.cs
@@ -3,26 +3,47 @@
 
 namespace EventsChallenge
 {
-    class PiggyBank {
+    class BalanceChangeEventArgs : EventArgs
+    {
+        public required decimal Old { get; set; }
+        public required decimal New { get; set; }
+
+    }
+    class PiggyBank
+    {
         private decimal _BalanceAmount;
 
-        public decimal TheBalance {
-            set {
+        public event EventHandler<BalanceChangeEventArgs>? BalanceChangeEvent;
+
+        public decimal TheBalance
+        {
+            set
+            {
+                if (BalanceChangeEvent is not null)
+                    BalanceChangeEvent(this, new BalanceChangeEventArgs() { Old = _BalanceAmount, New = value });
                 _BalanceAmount = value;
             }
-            get {
+            get
+            {
                 return _BalanceAmount;
             }
         }
     }
 
-    class Program {
-        static void Main(string[] args) {
+    class Program
+    {
+        static void Main(string[] args)
+        {
             decimal[] testValues = { 250, 1000, -750, 100, -200 };
-            
-            PiggyBank pb = new PiggyBank();
 
-            foreach (decimal testValue in testValues) {
+            PiggyBank pb = new PiggyBank();
+            pb.BalanceChangeEvent += delegate (object sender, BalanceChangeEventArgs e)
+            {
+                Console.WriteLine($"Balance changed from {e.Old} to {e.New}");
+            };
+
+            foreach (decimal testValue in testValues)
+            {
                 pb.TheBalance += testValue;
             }
             Console.WriteLine($"Final value is {pb.TheBalance}");
